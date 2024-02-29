53441ca57f95370cccf53cae9da419ddfc607afb
YUERONG HUANG <rowanne.h@hotmail.com>
2024-02-29 20:57:45 +1100

    set variables and add evenlistener to form and other buttons; fix autocomplete isses for form

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
 document.addEventListener('DOMContentLoaded', () => {
-    alert('see')
+    let form = document.querySelector('form');
+    let uBookings = document.querySelector('#upcoming-bookings');
+    let cBookings = document.querySelector('#cancelled-bookings');
+    let eBookings = document.querySelector('#expired-bookings');
+    let display = document.querySelector('#display');
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        console.log('e')
+        display.innerHTML = 'GO'
+    }
+
+    form.addEventListener('submit', handleSubmit)
+    uBookings.addEventListener('click', () =>{
+        alert('u')
+    })
+    cBookings.addEventListener('click', () =>{
+        alert('c')
+    })
+    eBookings.addEventListener('click', () =>{
+        alert('e')
+    })
+
+
 })
