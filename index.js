0c59298032212a610f038c36427f2bef33f538cb
YUERONG HUANG <rowanne.h@hotmail.com>
2024-02-29 22:35:57 +1100

    handleSumbit function: create an obj with form values when submit button is pressed

--- index.js
+++ index.js
@@ -7,20 +7,32 @@ document.addEventListener('DOMContentLoaded', () => {
 
     function handleSubmit(e) {
         e.preventDefault();
-        console.log('e')
+        let booking = {
+            date: e.target.date.value,
+            meals: e.target.meals.value,
+            no: e.target.number.value,
+            fname: e.target.fname.value,
+            lnam: e.target.lname.value,
+            mobile: e.target.mobile.value,
+            email: e.target.email.value,
+        }
         
+        console.log(booking)
     }
 
     form.addEventListener('submit', handleSubmit)
     uBookings.addEventListener('click', () =>{
         display.innerHTML = 'u'
     })
+
     cBookings.addEventListener('click', () =>{
         display.innerHTML = 'c'
     })
+
     eBookings.addEventListener('click', () =>{
         display.innerHTML = 'e'
     })
+
 
 
 })
